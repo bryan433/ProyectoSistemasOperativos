4d89d8cc28910504ca229a9a7028924ed892a878
bryan433 <bryan@Bryan.>
2026-07-06 22:42:49 -0400

    nose

--- servidor-3-datos/db-init/init.sh
+++ servidor-3-datos/db-init/init.sh
@@ -14,27 +14,27 @@ export PGPASSWORD=$PGPASSWORD
 
 # Crear la base de datos principal si no existe (ya que Patroni no usa POSTGRES_DB igual que el entrypoint normal)
 echo ">>> Ejecutando: CREATE DATABASE db_pagos_atencion..."
-psql -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -d postgres -c "CREATE DATABASE db_pagos_atencion OWNER admin_salud;" || echo "La DB db_pagos_atencion ya existe."
+psql -v ON_ERROR_STOP=1 -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -d postgres -c "CREATE DATABASE db_pagos_atencion OWNER admin_salud;" || echo "La DB db_pagos_atencion ya existe."
 
 # Crear la segunda base de datos
 echo ">>> Ejecutando: 01_create_db_medico.sql..."
-psql -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -d postgres -f /scripts/01_create_db_medico.sql || echo "La DB db_medico ya existe."
+psql -v ON_ERROR_STOP=1 -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -d postgres -f /scripts/01_create_db_medico.sql || echo "La DB db_medico ya existe."
 
 # Poblar primera base de datos
 echo ">>> Ejecutando: 02_schema_pagos_atencion.sql..."
-psql -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -d db_pagos_atencion -f /scripts/02_schema_pagos_atencion.sql
+psql -v ON_ERROR_STOP=1 -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -d db_pagos_atencion -f /scripts/02_schema_pagos_atencion.sql
 
 # Poblar segunda base de datos
 echo ">>> Ejecutando: 03_schema_medico.sql..."
-psql -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -d db_medico -f /scripts/03_schema_medico.sql
+psql -v ON_ERROR_STOP=1 -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -d db_medico -f /scripts/03_schema_medico.sql
 
 # Crear la tercera base de datos (Laboratorio)
 echo ">>> Ejecutando: 04_create_db_laboratorio.sql..."
-psql -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -d postgres -f /scripts/04_create_db_laboratorio.sql || echo "La DB db_laboratorio ya existe."
+psql -v ON_ERROR_STOP=1 -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -d postgres -f /scripts/04_create_db_laboratorio.sql || echo "La DB db_laboratorio ya existe."
 
 # Poblar tercera base de datos
 echo ">>> Ejecutando: 05_schema_laboratorio.sql..."
-psql -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -d db_laboratorio -f /scripts/05_schema_laboratorio.sql
+psql -v ON_ERROR_STOP=1 -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -d db_laboratorio -f /scripts/05_schema_laboratorio.sql
 
 echo ">>> ¡Inicialización de base de datos finalizada con éxito!"
 # Mantener el contenedor vivo o salir. Saldremos para que compose lo marque como 'exited' de forma limpia.
